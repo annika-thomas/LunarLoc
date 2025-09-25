087ad0215fcf9faa08a99c68e994c84ca90fd3b2
Annika <anni.thomas98@gmail.com>
2025-09-25 14:59:55 -0400

    updates to running on new computer

---
+++ lunarloc/pgo/extract_poses.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import copy
+import matplotlib as mpl
+mpl.use("Agg")  # set backend before importing pyplot/evo
+# from evo.tools import plot
+
+from evo.core import sync
+from evo.core import metrics
+from evo.tools import plot
+from evo.core.trajectory import PoseTrajectory3D
+
+from utils.datasets import extract_orbslam, extract_gt
+
+from lac_data import FrameDataReader
+
+
+def main(traverse: FrameDataReader, silent: bool = False):
+    orbslam_estimates, orbslam_frames = extract_orbslam(traverse)
+    gt_traj, gt_frames = extract_gt(traverse)
+
+    print("first gt: ", gt_traj[2])
+    print("first orb: ", orbslam_estimates[2])
+
+    traj_ref = PoseTrajectory3D(poses_se3=gt_traj, timestamps=gt_frames.astype(float))
+    traj_est = PoseTrajectory3D(
+        poses_se3=orbslam_estimates, timestamps=orbslam_frames.astype(float)
+    )
+
+    traj_ref, traj_est = sync.associate_trajectories(traj_ref, traj_est)
+    # print("traj ref")
+
+    traj_est_aligned = copy.deepcopy(traj_est)
+    traj_est_aligned.align(traj_ref, correct_scale=False, correct_only_scale=False)
+
+    data = (traj_ref, traj_est_aligned)
+    pose_relation = metrics.PoseRelation.translation_part
+    ape_metric = metrics.APE(pose_relation)
+    ape_metric.process_data(data)
+    ape_stats = ape_metric.get_all_statistics()
+
+    seconds_from_start = [t - traj_est.timestamps[0] for t in traj_est.timestamps]
+    fig = plt.figure()
+    plot.error_array(
+        fig.gca(),
+        ape_metric.error,
+        x_array=seconds_from_start,
+        statistics={s: v for s, v in ape_stats.items() if s != "sse"},
+        name="APE",
+        title="APE w.r.t. " + ape_metric.pose_relation.value,
+        xlabel="$t$ (s)",
+    )
+    
+    # savepath = f"outputs/APE_{traverse.metadata['description'].replace(' ', '_')}.png"
+    # plt.savefig(savepath)
+    # print(f"Plot created at: {savepath}")
+    # if not silent:
+    #     plt.show()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("-t", type=str, help="First agent traverse", required=True)
+    parser.add_argument("-s", help="If true, doesnt show plots", action="store_true")
+    args = parser.parse_args()
+
+    lac_path = Path("data")
+    # first_traverse = FrameDataReader(str(lac_path / args.t))
+    first_traverse = FrameDataReader("/home/annika/Downloads/data/orbslam_straight_line_preset1_default_20250917_102024.lac")
+    assert "orbslam" in first_traverse.custom_records.keys()
+
+    main(first_traverse, args.s)
--- lunarloc/pgo/run_pgo.py
+++ lunarloc/pgo/run_pgo.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 
 import numpy as np
@@ -8,22 +9,27 @@ import gtsam
 from gtsam.symbol_shorthand import X
 
 from lac_data import FrameDataReader
-from utils.plot import plot_initial_final
+# from utils.plot import plot_initial_final
 from utils.datasets import tf_at_frame
 
 # Noise is rotation-first, then translation
+# ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
+#     np.array([0.001, 0.001, 0.001, 0.005, 0.005, 0.005])
+# )  # ~0.5 deg, 5cm
 ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.001, 0.001, 0.001, 0.005, 0.005, 0.005])
-)  # ~0.5 deg, 5cm
-PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.0005, 0.0005, 0.0005, 0.001, 0.001, 0.001])
-)  # ~0.02 deg, 1cm
-LC_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.000005, 0.000005, 0.000005, 0.00005, 0.00005, 0.00005])
-)  # ~3 deg, 50cm
+    np.array([0.001, 0.001, 0.001, 0.005, 0.005, 0.005], dtype=np.float64)
+)
+
+# PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
+#     np.array([0.0005, 0.0005, 0.0005, 0.001, 0.001, 0.001])
+# )  # ~0.02 deg, 1cm
+# LC_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
+#     np.array([0.000005, 0.000005, 0.000005, 0.00005, 0.00005, 0.00005])
+# )  # ~3 deg, 50cm
 
 
 def main(first_traverse: FrameDataReader, silent: bool = False):
+    print("hmmm")
     # Read orbslam from the traverse
     orbslam_df = first_traverse.custom_records["orbslam"]
     orbslam_frames = orbslam_df["frame"].to_numpy()
@@ -33,75 +39,77 @@ def main(first_traverse: FrameDataReader, silent: bool = False):
     graph = gtsam.NonlinearFactorGraph()
     initial = gtsam.Values()
 
-    # Between factors
-    total_frames = len(orbslam_frames)
-    for i in range(total_frames - 1):
-        T_w_cur = orbslam_estimates[i]
-        T_w_next = orbslam_estimates[i + 1]
-        T_cur_next = pyt_t.invert_transform(T_w_cur) @ T_w_next
+    # # Between factors
+    # total_frames = len(orbslam_frames)
+    # for i in range(total_frames - 1):
+    #     T_w_cur = orbslam_estimates[i]
+    #     T_w_next = orbslam_estimates[i + 1]
+    #     T_cur_next = pyt_t.invert_transform(T_w_cur) @ T_w_next
 
-        graph.add(
-            gtsam.BetweenFactorPose3(
-                X(i), X(i + 1), gtsam.Pose3(T_cur_next), ODOMETRY_NOISE
-            )
-        )
+    #     graph.add(
+    #         gtsam.BetweenFactorPose3(
+    #             X(i), X(i + 1), gtsam.Pose3(T_cur_next), ODOMETRY_NOISE
+    #         )
+    #     )
 
-    # Initial estimate is raw orbslam estimate
-    for i, estimate in enumerate(orbslam_estimates):
-        initial.insert(X(i), gtsam.Pose3(estimate))
+    # # Initial estimate is raw orbslam estimate
+    # for i, estimate in enumerate(orbslam_estimates):
+    #     initial.insert(X(i), gtsam.Pose3(estimate))
 
-    # Set prior to GT first pose so they align when plotting
-    graph.add(
-        gtsam.PriorFactorPose3(
-            X(0),
-            gtsam.Pose3(tf_at_frame(first_traverse[orbslam_frames[0]])),
-            PRIOR_NOISE,
-        )
-    )
+    # # Set prior to GT first pose so they align when plotting
+    # graph.add(
+    #     gtsam.PriorFactorPose3(
+    #         X(0),
+    #         gtsam.Pose3(tf_at_frame(first_traverse[orbslam_frames[0]])),
+    #         PRIOR_NOISE,
+    #     )
+    # )
 
-    # Add dummy loop closures
-    for i in range(1, total_frames - 1, 100):
-        for j in range(1, total_frames - 1, 100):
-            if i != j:
-                T_w_i = tf_at_frame(first_traverse[orbslam_frames[i]])
-                T_w_j = tf_at_frame(first_traverse[orbslam_frames[j]])
-                T_i_j = pyt_t.invert_transform(T_w_i) @ T_w_j
+    # # Add dummy loop closures
+    # for i in range(1, total_frames - 1, 100):
+    #     for j in range(1, total_frames - 1, 100):
+    #         if i != j:
+    #             T_w_i = tf_at_frame(first_traverse[orbslam_frames[i]])
+    #             T_w_j = tf_at_frame(first_traverse[orbslam_frames[j]])
+    #             T_i_j = pyt_t.invert_transform(T_w_i) @ T_w_j
 
-                graph.add(
-                    gtsam.BetweenFactorPose3(
-                        X(i - 1), X(j - 1), gtsam.Pose3(T_i_j), LC_NOISE
-                    )
-                )
+    #             graph.add(
+    #                 gtsam.BetweenFactorPose3(
+    #                     X(i - 1), X(j - 1), gtsam.Pose3(T_i_j), LC_NOISE
+    #                 )
+    #             )
 
-    # Optimize
-    params = gtsam.LevenbergMarquardtParams()
-    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
-    result = optimizer.optimize()
+    # # Optimize
+    # params = gtsam.LevenbergMarquardtParams()
+    # optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
+    # result = optimizer.optimize()
 
-    print("Optimization complete!")
-    print("final graph error = ", graph.error(result))
+    # print("Optimization complete!")
+    # print("final graph error = ", graph.error(result))
 
-    plot_initial_final(first_traverse, initial, result, total_frames)
+    # plot_initial_final(first_traverse, initial, result, total_frames)
 
-    savepath = (
-        f"outputs/PGO_{first_traverse.metadata['description'].replace(' ', '_')}.png"
-    )
-    plt.savefig(savepath)
-    print(f"Plot created at: {savepath}")
-    if not silent:
-        plt.show()
+    # savepath = (
+    #     f"outputs/PGO_{first_traverse.metadata['description'].replace(' ', '_')}.png"
+    # )
+    # plt.savefig(savepath)
+    # print(f"Plot created at: {savepath}")
+    # if not silent:
+    #     plt.show()
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", type=str, help="First agent traverse", required=True)
-    parser.add_argument("-s", help="If true, doesnt show plots", action="store_true")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # # parser.add_argument("-t", type=str, help="First agent traverse", required=True)
+    # parser.add_argument("-s", help="If true, doesnt show plots", action="store_true")
+    # args = parser.parse_args()
 
-    lac_path = Path("data")
-    first_traverse = FrameDataReader(str(lac_path / args.t))
-    assert "orbslam" in first_traverse.custom_records.keys()
+    # lac_path = Path("data")
+    # first_traverse = FrameDataReader(str(lac_path / args.t))
+    first_traverse = FrameDataReader("/home/annika/Downloads/data/orbslam_straight_line_preset1_default_20250917_102024.lac")
 
-    main(first_traverse, args.s)
+    # assert "orbslam" in first_traverse.custom_records.keys()
+
+    main(first_traverse)
